361c905a7a6cce2c355cc240434226c7e7bc3d7f
Ralphael Obadiaru <ralph.obadiaru@gmail.com>
2024-03-18 15:18:20 +0000

    task 3 done

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 // Event Listeners
 
 const form = document.querySelector("#todo-form");
-
 const list = document.querySelector("#list");
+const input = document.querySelector("#new-todo");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // create list item
-  const listItem1 = document.createElement("li");
-  listItem1.innerText = "List item 1";
+  const listItem = document.createElement("li");
 
-  list.appendChild(listItem1);
+  // add input to list item
+  const inputValue = input.value;
+  listItem.innerText = inputValue;
+
+  // delete button
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", () => {
+    listItem.remove();
+  });
+
+  listItem.appendChild(deleteButton);
+
+  list.appendChild(listItem);
 });
